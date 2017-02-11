14eef2f8ca8f8dd553c796c9e0d83fc146375200
Efim Bogdanovsky <profit234@gmail.com>
2017-02-11 18:27:45 +0200

    Update solution.c

--- leetcode/easy/remove_linked_list_elements/c/solution.c
+++ leetcode/easy/remove_linked_list_elements/c/solution.c
@@ -1,9 +1,9 @@
 /*
  * The good ol'slow/fast pointer/index approach coupled with the dummy
  * technique for linked lists. Simply allocate a dummy (preferrably on
- * the stack) and append all nodes which do not contain the target value
- * to it. At the end of the traversal, the next pointer of the dummy node
- * will be pointing to the new head.
+ * the stack) and have its next pointer point to the head node. This
+ * will save us an extra case when the head node value is equal to the
+ * target value.
  *
  * O(n) time, O(1) space.
  */
@@ -26,11 +26,11 @@ struct ListNode* removeElements(struct ListNode* head, int val)
         
     while (fast != NULL) {
         if (fast->next != NULL && fast->next->val == val) {
-            fast = fast->next;   
+            fast       = fast->next;   
         } else {
             slow->next = fast->next;
-            fast = fast->next;
-            slow = slow->next;
+            fast       = fast->next;
+            slow       = slow->next;
         }
     }
     
