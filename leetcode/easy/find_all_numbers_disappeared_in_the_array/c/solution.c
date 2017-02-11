4279edc9a8e819f69aeae6583b3f4f42592ae9d6
Efim Bogdanovsky <profit234@gmail.com>
2017-02-11 12:09:59 +0200

    Update solution.c

--- leetcode/easy/find_all_numbers_disappeared_in_the_array/c/solution.c
+++ leetcode/easy/find_all_numbers_disappeared_in_the_array/c/solution.c
@@ -16,14 +16,20 @@ int* findDisappearedNumbers(int* nums, int numsSize, int* returnSize) {
     int *buffer = (int*)malloc(sizeof(nums[0]) * numsSize);
     if (buffer)
     {
+        // Sort the array to group up the duplicates.
         qsort(nums, numsSize, sizeof(nums[0]), compareIntegers);
         
         for (int index = 0, value = startValue; value <= endValue; ++value) 
         {
+            // If we are faced with a duplicate, simply ignore it, and move 
+            // a step forward.
             if (index > 0 && nums[index] == nums[index - 1]) {
                 ++index;
             }
             
+            // Here we should run into a number which is equal to the value.
+            // If not, the value is not in the array, hence classified as one
+            // of the "disappeared" numbers.
             if (nums[index] != value) {
                 buffer[(*returnSize)++] = value;
             } else {
