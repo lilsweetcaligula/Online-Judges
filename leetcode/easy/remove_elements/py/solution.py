f6a6ffcd8b27a7a0b5d9dc9078149324b743673b
Efim Bogdanovsky <profit234@gmail.com>
2017-02-11 18:02:45 +0200

    Update solution.py

--- leetcode/easy/remove_elements/py/solution.py
+++ leetcode/easy/remove_elements/py/solution.py
@@ -2,8 +2,11 @@
 # Here at Python Solutions Inc. we like to present you with
 # ways to get the job done on a lazy sleepless Monday in Seattle.
 #
-# Use filter to filter out target values - O(n) space. Copy the
-# result back to nums. O(2n) time complexity, or simply O(n).
+# Use the built-in filter function to filter out the target values.
+# This will be an O(n) space. 
+#
+# Copy the result back to nums. This will be O(2n) time complexity, 
+# or simply O(n).
 #
 # Return the new length of the nums as required by the problem
 # statement.
