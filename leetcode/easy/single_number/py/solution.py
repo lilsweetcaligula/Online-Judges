59032cc1d0b06e0e8b688ecf568557de0cc7caf0
Efim Bogdanovsky <profit234@gmail.com>
2017-02-11 11:12:23 +0200

    Update solution.py

--- leetcode/easy/single_number/py/solution.py
+++ leetcode/easy/single_number/py/solution.py
@@ -1,3 +1,18 @@
+#
+# The functional approach we could use to locate the single number is
+# to group the numbers by value and return the group with only a single 
+# number in it.
+#
+# The caveat to the groupping approach is we need a sorted array for
+# best performance - O(n log n). Hence: itertools.groupby(sorted(nums))
+#
+# A more costly alternative would be to use the key predicate taken
+# by the itertools.groupby function, which gets the number's count
+# via the list.count method. The time complexity of list.count method
+# is O(n). When applied to n numbers, the overall solution complexity
+# results in the unacceptable O(n^2).
+#
+
 class Solution(object):
     def singleNumber(self, nums):
         """
@@ -5,6 +20,7 @@ class Solution(object):
         :rtype: int
         """
         import itertools
+        # https://docs.python.org/3/library/itertools.html
         
         for num, group in itertools.groupby(sorted(nums)):
             count = len(tuple(group))
@@ -13,3 +29,4 @@ class Solution(object):
                 return num
                 
         return None
+    
