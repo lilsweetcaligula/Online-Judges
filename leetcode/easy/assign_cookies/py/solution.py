ad633cd08488393f096e2f1505d94818f82e988c
Efim Bogdanovsky <profit234@gmail.com>
2017-02-12 12:27:11 +0200

    Create solution.py

---
+++ leetcode/easy/assign_cookies/py/solution.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def findContentChildren(self, g, s):
+        """
+        :type g: List[int]
+        :type s: List[int]
+        :rtype: int
+        """
+        g, s  = sorted(g), sorted(s)        
+        i     = 0
+        j     = 0
+        count = 0
+        
+        while i < len(g) and j < len(s):
+            if g[i] <= s[j]:
+                count += 1
+                i += 1
+            j += 1
+            
+        return count
