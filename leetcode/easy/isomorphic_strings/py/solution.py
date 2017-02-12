b57474ba5a3ea92c300c30f51f6df64382ffd2a2
Efim Bogdanovsky <profit234@gmail.com>
2017-02-12 11:30:52 +0200

    Create solution.py

---
+++ leetcode/easy/isomorphic_strings/py/solution.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False
+            
+        #
+        # Leetcode hasn't upgraded its Python judges to 3.x, however the function
+        # still receives unicode strings as parameters. str.translate method in
+        # Python 2.x will not work on a unicode translation table, thus we must
+        # build a translation table ourselves based on char codes.
+        #
+        return (    t == s.translate({ ord(frm): ord(to) for frm, to in zip(s, t)})
+                and s == t.translate({ ord(frm): ord(to) for frm, to in zip(t, s)}))
+                
+        # A cleaner Py3 solution would be:
+        #
+        # return t == s.translate(str.maketrans(s, t)) and s == t.translate(str.maketrans(t, s))
