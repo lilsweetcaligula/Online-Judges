01787955a236d7d4fbc93a204a297231f0cca05e
Efim Bogdanovsky <profit234@gmail.com>
2017-02-13 04:53:28 +0200

    Create solution.py

---
+++ leetcode/easy/convert_a_number_to_a_hexadecimal/solution.py
@@ -0,0 +1,25 @@
+#
+# Yeah, we could just use the built-in "hex" function, but where's the challenge in that?
+#
+
+class Solution(object):
+    def toHex(self, number):
+        """
+        :type number: int
+        :rtype: str
+        """
+        import ctypes
+        
+        HEX_LOOKUP = '0123456789abcdef'
+        BASE       = 16
+        result     = ''
+        
+        number     = ctypes.c_uint32(number).value
+        
+        while True:
+            result  += HEX_LOOKUP[number % BASE]
+            number //= BASE
+            if number == 0:
+                break
+            
+        return result[::-1]
