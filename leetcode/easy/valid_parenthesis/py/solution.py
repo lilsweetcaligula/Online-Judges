f34a0bb0c3bbc670c9b0fbcabfd00bf5024be183
Efim Bogdanovsky <profit234@gmail.com>
2017-02-13 04:57:06 +0200

    Create solution.py

---
+++ leetcode/easy/valid_parenthesis/py/solution.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        LPARENS = {
+            '(': ')', '{': '}', '[': ']'   
+        }
+        
+        RPARENS = {
+            ')': '(', '}': '{', ']': '['
+        }
+        
+        stack = []
+        
+        for c in s:
+            
+            if c in LPARENS:
+                stack.append(c)
+                
+            elif c in RPARENS:
+                if len(stack) == 0:
+                    return False
+                    
+                t = stack.pop()
+                
+                if RPARENS[c] != t:
+                    return False
+                    
+        return len(stack) == 0
