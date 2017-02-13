19584e91b1e6f8e371ad83dffae12f67e05dab7b
Efim Bogdanovsky <profit234@gmail.com>
2017-02-13 12:39:39 +0200

    Update solution.py

--- hackerrank/algorithms/implementation/easy/sock_merchant/py/solution.py
+++ hackerrank/algorithms/implementation/easy/sock_merchant/py/solution.py
@@ -4,7 +4,7 @@ import sys
 import collections
 
 n = int(input())
-c = map(int, input().split(' '))
+c = map(int, input().split())
 
 pairCount = sum(count // 2 for count in collections.Counter(c).values())
 
