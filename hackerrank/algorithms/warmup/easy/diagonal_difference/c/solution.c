c920da912f8a4545f26cec4be24b3e4151781bb0
Efim Bogdanovsky <profit234@gmail.com>
2017-02-13 10:30:46 +0200

    Update solution.c

--- hackerrank/algorithms/warmup/easy/diagonal_difference/c/solution.c
+++ hackerrank/algorithms/warmup/easy/diagonal_difference/c/solution.c
@@ -21,7 +21,7 @@ int main(void)
     int sum1 = 0, sum2 = 0;
     for (int a_i = 0; a_i < n; a_i++) {
         for (int a_j = 0; a_j < n; a_j++) {
-            if (a_i == a_j) sum1 += a[a_i][a_j];            
+            if (a_i == a_j) sum1 += a[a_i][a_j];
             if (a_j + a_i == n - 1) sum2 += a[a_i][a_j];
         }
     }
