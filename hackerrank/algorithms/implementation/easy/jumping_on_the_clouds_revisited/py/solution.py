3dcee94f8cc0a26aaeea2948e900440838b1d4f5
Efim Bogdanovsky <profit234@gmail.com>
2017-02-13 12:57:02 +0200

    Update solution.py

--- hackerrank/algorithms/implementation/easy/jumping_on_the_clouds_revisited/py/solution.py
+++ hackerrank/algorithms/implementation/easy/jumping_on_the_clouds_revisited/py/solution.py
@@ -7,10 +7,8 @@ PENALTY_JUMP        = 1
 
 e    = 100
 
-n, k = input().strip().split(' ')
-n, k = [int(n),int(k)]
-c    = [int(c_temp) for c_temp in input().strip().split(' ')]
-
+n, k = map(int, input().split())
+c    = tuple(map(int, input().split()))
 i    = 0
 
 while True:
