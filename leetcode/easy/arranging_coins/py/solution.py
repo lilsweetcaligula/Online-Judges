951a03bb5e6d036fbde7fdf30b43172f6adc8bfd
Efim Bogdanovsky <profit234@gmail.com>
2017-02-13 09:00:16 +0200

    Update solution.py

--- leetcode/easy/arranging_coins/py/solution.py
+++ leetcode/easy/arranging_coins/py/solution.py
@@ -1,7 +1,16 @@
-[href.] https://leetcode.com/problems/arranging-coins/
-
-You have a total of n coins that you want to form in a staircase shape, where every k-th row must have exactly k coins.
-
-Given n, find the total number of full staircase rows that can be formed.
-
-n is a non-negative integer and fits within the range of a 32-bit signed integer.
+class Solution(object):
+    def arrangeCoins(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        x   = n
+        y   = 1
+        cnt = 0
+    
+        while y <= x:
+            x   -= y
+            y   += 1
+            cnt += 1
+    
+        return cnt
