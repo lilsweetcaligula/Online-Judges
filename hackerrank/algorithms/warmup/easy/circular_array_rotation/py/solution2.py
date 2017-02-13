45ef2ec2d253d59083518900f9509ce6de57c416
Efim Bogdanovsky <profit234@gmail.com>
2017-02-13 10:45:53 +0200

    Create solution2.py

---
+++ hackerrank/algorithms/warmup/easy/circular_array_rotation/py/solution2.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+
+import sys
+
+def rotr(seq, cnt):
+    if len(seq) == 0:
+        return seq[:]
+
+    k         = cnt % len(seq)
+    seq[:]    = reversed(seq)
+    seq[:k] = reversed(seq[:k])
+    seq[k:] = reversed(seq[k:])
+
+    return seq
+
+n,k,q = input().strip().split(' ')
+n,k,q = map(int, (n, k, q))
+a     = list(map(int, input().strip().split(' ')[:n]))
+rot   = rotr(a, k)
+
+for a0 in range(q):
+    m = int(input().strip())
+    
+    print(rot[m])
