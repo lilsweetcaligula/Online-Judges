887def51fcab084b0f84a56b4155d9e4caf98878
Efim Bogdanovsky <profit234@gmail.com>
2017-02-13 08:45:45 +0200

    Create solution.py

---
+++ leetcode/easy/range_sum_query_immutable/py/solution.py
@@ -0,0 +1,27 @@
+class NumArray(object):
+
+    def __init__(self, nums):
+        """
+        :type nums: List[int]
+        """
+        
+        self._sums = [0]
+        total      = 0
+        
+        for num in nums:
+            total += num
+            
+            self._sums.append(total)
+
+    def sumRange(self, i, j):
+        """
+        :type i: int
+        :type j: int
+        :rtype: int
+        """
+        return self._sums[j + 1] - self._sums[i]
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(i,j)
