a984da25383da5ac34cb55a914769a5ac6c50907
Efim Bogdanovsky <profit234@gmail.com>
2017-02-13 13:04:03 +0200

    Create solution.py

---
+++ hackerrank/algorithms/implementation/easy/append_and_delete/py/solution.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import sys
+
+def canConvert(s, t, k):    
+    cmnPrefixCnt = 0
+
+    for c1, c2 in tuple(zip(s, t)):
+        if c1 == c2:
+            cmnPrefixCnt += 1
+        else:
+            break
+
+    minDelCnt = len(s) - cmnPrefixCnt
+    minAddCnt = len(t) - cmnPrefixCnt
+    
+    if k >= len(s) + len(t):
+        return True
+    else:    
+        addCnt = minAddCnt
+
+        for delCnt in range(minDelCnt, len(s) + 1):
+            chgCnt = addCnt + delCnt
+
+            if chgCnt == k:
+                return True
+            elif chgCnt > k:
+                return False
+
+            addCnt += 1
+
+    return False
+    
+s = input().strip()
+t = input().strip()
+k = int(input().strip())
+
+if canConvert(s, t, k):
+    print('Yes')
+else:
+    print('No')
