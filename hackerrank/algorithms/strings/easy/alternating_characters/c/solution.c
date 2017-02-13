c4324aa7907ef8c21e21bb2f5c8e1b5e46de8c08
Efim Bogdanovsky <profit234@gmail.com>
2017-02-13 19:11:21 +0200

    Update solution.c

--- hackerrank/algorithms/strings/easy/alternating_characters/c/solution.c
+++ hackerrank/algorithms/strings/easy/alternating_characters/c/solution.c
@@ -10,13 +10,11 @@ int main(void)
     
     while ((c = getchar()) && isdigit(c)) {
         testcount = testcount * 10 + c - '0';
-    }
-    
-    char curr = '\0', prev = '\0';
+    }        
     
     for (int i = 0; i < testcount; ++i) 
     {
-        curr = '\0', prev = '\0';
+        char curr = '\0', prev = '\0';
         int count = 0;
         
         while ((curr = getchar()) != '\n' && curr != EOF) {
@@ -29,5 +27,5 @@ int main(void)
         printf("%d\n", count);
     }
     
-    return 0;
+    return EXIT_SUCCESS;
 }
