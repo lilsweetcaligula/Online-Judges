5d1aff252d31c384a0dcbc0c46b3e7fc0521e4c7
Efim Bogdanovsky <efim.bogdanovsky@gmail.com>
2017-03-07 05:19:49 +0200

    Create sort_integers.cpp

---
+++ lintcode/naive/sort_integers/cpp/sort_integers.cpp
@@ -0,0 +1,28 @@
+#include <algorithm>
+
+class Solution {
+public:
+    /**
+     * @param A an integer array
+     * @return void
+     */
+    void sortIntegers(vector<int>& A) 
+    {
+        // Bubble sort.
+        
+        auto wasSwapped = false;
+        
+        for (decltype(A.size()) i{ 0 }; i < A.size(); ++i) 
+        {
+            for (auto j = i; j < A.size(); ++j) 
+            {
+                if (A[j] < A[i]) {
+                    std::swap(A[i], A[j]);
+                    wasSwapped = true;
+                }
+            }
+            
+            if (!wasSwapped) break;
+        }
+    }
+};
