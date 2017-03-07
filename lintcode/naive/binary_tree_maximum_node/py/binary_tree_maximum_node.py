540def45f3bcfc096709f42e23cbb7067d6b9ba0
Efim Bogdanovsky <efim.bogdanovsky@gmail.com>
2017-03-07 02:55:04 +0200

    Create binary_tree_maximum_node.py

---
+++ lintcode/naive/binary_tree_maximum_node/py/binary_tree_maximum_node.py
@@ -0,0 +1,20 @@
+class Solution:
+    # @param {TreeNode} root the root of binary tree
+    # @return {TreeNode} the max node
+    def maxNode(self, root):
+        def helper(root, curmax):
+            if root == None:
+                return curmax
+                
+            curmax = max([root, curmax], key=lambda x: x.val)
+            
+            return max(
+                curmax, 
+                helper(root.left, curmax), 
+                helper(root.right, curmax),
+                key=lambda x: x.val)
+            
+        if root == None:
+            return None
+        
+        return helper(root, root)
